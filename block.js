27dce64022ddc3756cb840150958e3275e09edab
Shubham2223 <73734918+Shubham2223@users.noreply.github.com>
2020-12-28 19:20:59 +0530

    Add files via upload

---
+++ block.js
@@ -0,0 +1,47 @@
+class Block{
+    constructor(x,y,width,height){
+        var options={
+           
+            restitution:0.4,
+            friction:0.0,
+           
+        }
+        this.body=Bodies.rectangle(x,y,width,height,options);
+        this.width=width;
+        this.height=height;
+        this.Visibility=255;
+        World.add(world,this.body);
+
+    }
+    display(){
+       var angle =this.body.angle;
+        var pos =this.body.position;
+        push();
+        rotate(angle);
+        translate(pos.x,pos.y);
+        rectMode(CENTER);
+        rect(0,0,this.width,this.height);
+        pop();
+
+         if(this.body.speed<3){
+            rect(pos.x,pos.y,this.width,this.height);
+        }
+        else{
+          
+          World.remove(world,this.body);
+          push()
+          rotate(angle);
+          this.Visibility=this.Visibility-5
+          tint(255,this.Visibility);
+          rect(this.rect,pos.x,pos.y,30,40);
+          pop();
+  
+        }
+
+        
+     
+       
+      
+    
+    }
+};
